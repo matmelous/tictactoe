88a3a673b66553267edba214a49d038399da4925
Matheus Santos <matmelous@gmail.com>
2022-02-26 19:55:47 -0300

    First elements and styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,51 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='board'>
+        <div className="background">
+          <div className="line line1"></div>
+          <div className="line line2"></div>
+          <div className="line line3"></div>
+          <div className="line line4"></div>
+        </div>
+        <div className='board-square'>
+          <div className="piece cross">
+            <div className="x1">
+              <div className="sub neon"></div>
+              <div className="sub neon"></div>
+            </div>
+            <div className="x2 neon"></div>
+          </div>
+
+        </div>
+        <div className='board-square'>
+          <div className="piece round"> </div>
+        </div>
+        <div className='board-square'>
+          <div className="piece round"> </div>
+        </div>
+        <div className='board-square'>
+          <div className="piece round"> </div>
+        </div>
+        <div className='board-square'>
+          <div className="piece round"> </div>
+        </div>
+        <div className='board-square'>
+          <div className="piece round"> </div>
+        </div>
+        <div className='board-square'>
+          <div className="piece round"> </div>
+        </div>
+        <div className='board-square'>
+          <div className="piece round"> </div>
+        </div>
+        <div className='board-square'>
+          <div className="piece round"> </div>
+        </div>
+
+      </div>
+
     </div>
   );
 }
